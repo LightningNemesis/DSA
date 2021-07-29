7b220a943dd5b78fa12ac167542ce64e167756ed
Abhishek <ak.kumar220919@gmail.com>
2021-07-29 20:53:01 +0530

    Added tabulation for coin combination

--- src/DP/coin_denominations_sum/coin_denom_sum.java
+++ src/DP/coin_denominations_sum/coin_denom_sum.java
@@ -4,8 +4,8 @@
  * 2: 2+3
  * 5: 5
  */
-package DP.coin_denominations_sum;
 
+package DP.coin_denominations_sum;
 import InputHandler.InputHandler;
 import java.util.*;
 
@@ -17,7 +17,7 @@ public class coin_denom_sum extends InputHandler {
         int[] arr = Arrays.stream(br.readLine().split("\\s*,\\s*")).mapToInt(Integer::parseInt).toArray();
         System.out.println("Enter sum");
         int sum = Integer.parseInt(br.readLine());
-        System.out.println(new Solver(arr.length, sum).count_combinations_woutdp(arr, arr.length, sum));
+        System.out.println(new Solver(arr.length, sum).tabulation(arr, arr.length, sum));
     }
 }
 
@@ -33,26 +33,36 @@ class Solver{
         }
     }
 
-    int count_combinations_woutdp(int arr[], int m, int sum){
+    int recursion(int arr[], int m, int sum){
         if(sum == 0) return 1; //We have found 1 combination
         if(m==0) return 0; //We don't have coins left for any other combination
 
-        int res = count_combinations_woutdp(arr, m-1, sum); // Not including the denomination
+        int res = recursion(arr, m-1, sum); // Not including the denomination
         if(arr[m-1]<=sum)
-            res = res + count_combinations_woutdp(arr, m, sum-arr[m-1]); //including the denomination & reducing sum
+            res = res + recursion(arr, m, sum-arr[m-1]); //including the denomination & reducing sum
 
         return res;
     }
 
-    int count_combinations_wdp(int arr[], int m, int sum){
-
+    int memoization(int arr[], int m, int sum){
         if(dp[m][sum]==-1){
             if(sum == 0) dp[m][sum] = 1; //We have found 1 combination
             if(m==0) dp[m][sum]=0; //We don't have coins left for any other combination
 
-            dp[m][sum] = count_combinations_woutdp(arr, m-1, sum); // Not including the denomination
+            dp[m][sum] = memoization(arr, m-1, sum); // Not including the denomination
             if(arr[m-1]<=sum)
-                dp[m][sum] = dp[m][sum] + count_combinations_woutdp(arr, m, sum-arr[m-1]); //including the denomination & reducing sum
+                dp[m][sum] = dp[m][sum] + memoization(arr, m, sum-arr[m-1]); //including the denomination & reducing sum
+        }
+        return dp[m][sum];
+    }
+
+    int tabulation(int[]arr, int m, int sum){
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=sum; j++){
+                dp[i][j] = dp[i-1][j];
+                if(arr[i-1]<=j)
+                    dp[i][j] += dp[i][j-arr[i-1]];
+            }
         }
         return dp[m][sum];
     }
